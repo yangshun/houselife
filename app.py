6e5c96299e982773d08c519a60af62473a227aa1
Joe Chee <joe.chee@gmail.com>
2012-12-01 15:26:42 -0800

    created a first draft of the login form

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def frontPage():
-    return render_template('main.html')
+    return redirect(url_for('login'))
 
 
 @app.route("/login")
