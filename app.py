fb29bfd59c808851be6b8956cb40255866a40ae1
Joe Chee <joe.chee@gmail.com>
2012-12-01 14:25:56 -0800

    added some templates for me to work around with

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def frontPage():
+    return render_template('main.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
-
