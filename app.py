c4cf37c644d3f8c461e196d30856c7add83de581
hendychua <hendychua@gmail.com>
2012-12-01 15:40:16 -0800

    merging in changes

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
-from flask import Flask
-from flask import render_template, redirect, url_for
+import json
+import logging
+import os
+
+from flask import Flask, request, redirect, url_for, g, abort, session, \
+                    render_template
+import requests
+
 
 app = Flask(__name__)
 
+logging.basicConfig(format='%(levelname)s %(asctime)s:    '\
+                        '%(message)s', level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+HOST = "127.0.0.1"
+PORT = 5000
+DEBUG_MODE = True
 
 @app.route("/")
-def frontPage():
+def index():
+    log.info("Re-routing to login page from index.")
     return redirect(url_for('login'))
-
-
+    
 @app.route("/login")
 def login():
     return render_template('login.html')
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+def attach_blueprints_to_app():
+    from blueprints import user
+    app.register_blueprint(user.mod, url_prefix='/%s' % engagement.mod.name)
+
+if __name__ == '__main__':
+    attach_blueprints_to_app()
+
+    app.run(host=HOST, port=PORT, debug=DEBUG_MODE)
