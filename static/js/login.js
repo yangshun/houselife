6c400ce61128f6d5a724b97c535dec405f8ffad6
Joe Chee <joe.chee@gmail.com>
2012-12-01 22:56:58 -0800

    fixed login

--- static/js/login.js
+++ static/js/login.js
@@ -8,13 +8,14 @@ $(function () {
             'username': $('#login-username').val(),
             'password': $('#login-password').val()
         }, function(res) {
-            //TODO: change when endpoint is defined
-            var success = true;
-
+            var success;
+            if (res.code && res.code == 200) {
+                success = true;
+            }
             if (success) {
                 window.location.href = "/dashboard";
             } else {
-                showErrorMessage(res);
+                showErrorMessage(res.message);
             }
         });
     });
